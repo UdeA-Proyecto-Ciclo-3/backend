f5759dde96f0bbb0ddbd6e2aae2e0dade9d496d0
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-10-15 12:46:05 -0500

    [+] Controller /api/auth/google-login: Registra usuario Google (si no existe) y crea respectivo Token del sistema

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,3 +1,6 @@
+const jwt = require( 'jsonwebtoken' );
+require( 'dotenv' ) .config({ path: '.env' });
+
 const Usuario = require("../models/Usuario");
 
 const
@@ -8,6 +11,7 @@ const
 /** Crea nuevo recurso */
 exports.googleLogin = async (request, response) => {
     const { tokenId } = request .body;
+
     console.log( 'tokenId: ', tokenId );
 
     // ! Verify Google TokenID
@@ -19,18 +23,48 @@ exports.googleLogin = async (request, response) => {
         payload = ticket .getPayload(),
         { name, given_name, family_name, picture, email, email_verified } = payload;
 
+        let token = null;
+
     // ! Verificacion de correo
     if( email_verified ) {
-        const userFound = await Usuario .findOne({ correo: email });
+        const userFound = await Usuario .findOne({ correo: email }) .select( '-contrasena' );
 
-        // ! Verifica si el usuario encontrado esta registrado
+        // ! Verifica si el usuario encontrado esta registrado y lo registra
         if( ! userFound ) {
-            console.info( `Este usuario no existe, registrate primero.` );
+            const
+                contrasena = `${ email }${ process .env .SECRET }`,
+                newUser = new Usuario({ nombre: name, correo: email, rol: 'comprador', contrasena });
+
+            await newUser .save();
+
+            // ! Crea el Token el nuevo usuario registrado, usando JWT
+            token = jwt .sign(
+                { id: newUser._id },
+                process .env .SECRET,
+                { expiresIn: '1d' /** 24h */ }
+            );
+
             return response .json({
-                error: {
-                    mensaje: `Este usuario no existe, registrate primero.`
-                }
-            })}
+                mensaje: `El nuevo usuario ha sido registrado y autenticado`,
+                usuario: newUser,
+                token
+            })
         }
+
+        console.log( 'userFound: ', userFound );
+
+        // ! Crea el Token para un usuario que esta registrado, usando JWT
+        token = jwt .sign(
+            { id: userFound._id },
+            process .env .SECRET,
+            { expiresIn: '1d' /** 24h */ }
+        );
+
+        return response .json({
+            mensaje: `El usuario ha sido autenticado`,
+            usuario: userFound,
+            token
+        });
+    }
 
 };
