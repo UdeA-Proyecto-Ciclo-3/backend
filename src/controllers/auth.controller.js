5944ee2e2ca81f6c23fabba3ebb96784e2d4e699
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-10-15 11:51:34 -0500

    [+] Controller /api/auth/google-login/: Verificacion del Token para la Autenticacion del Usuario (OAuth2 Google)

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,10 +1,24 @@
 const Usuario = require("../models/Usuario");
 
+const
+    CLIENT_ID = '367337669931-s6d728je9j4p0nkq11u8r53s2an0hdeo.apps.googleusercontent.com',
+    { OAuth2Client } = require( 'google-auth-library' ),
+    client = new OAuth2Client( CLIENT_ID );
+
 /** Crea nuevo recurso */
 exports.googleLogin = async (request, response) => {
     const { tokenId } = request .body;
+    console.log( 'tokenId: ', tokenId );
 
-    console.log( 'tokenId', tokenId );
+    // ! Verify Google TokenID
+    const
+        ticket = await client .verifyIdToken({
+            idToken: tokenId,
+            audience: CLIENT_ID
+        }),
+        payload = ticket .getPayload();
+
+    console .log( payload );
 
     response .json( 'googleLogin' );
 };
