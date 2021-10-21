6521d06b5b05143f3ca62639f09be59c88f03f43
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-10-20 22:57:03 -0500

    [$] Mongoose: Configuracion conexion

--- src/config/db.js
+++ src/config/db.js
@@ -1,17 +1,21 @@
 const mongoose = require("mongoose");
 require("dotenv").config({ path: ".env" });
 
-const ConnectDatabase = async () => {
-  try {
-    await mongoose.connect(process.env.DB_MONGO_ATLAS, {
-      // useNewUrlParser: true,
-      // useUnifiedTopology: true,
+const ConnectDatabase = () => {
+
+    mongoose.connect( process.env.DB_MONGO_ATLAS );
+
+    // mongoose.set('useNewUrlParser', true);
+    // mongoose.set('useFindAndModify', false);
+    // mongoose.set('useCreateIndex', true);
+
+    mongoose.connection.once( 'open', () => {
+      console.log( 'MongoDB Connected' );
     });
-    console.log("Connected MongoDB");
-  } catch (error) {
-    console.log(error);
-    process.exit(1); // Detiene la app
-  }
+    mongoose.connection.on( 'error', (err) => {
+      console.log( 'MongoDB connection error: ', err );
+    });
+
 };
 
 module.exports = ConnectDatabase;
